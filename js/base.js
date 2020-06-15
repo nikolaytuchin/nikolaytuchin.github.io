db246a2cb9daa11629ee15d88f0f2fc306914bf9
nikolaytuchin <63854942+nikolaytuchin@users.noreply.github.com>
2020-06-15 16:19:30 +0500

    Create base.js

---
+++ js/base.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() { // когда весь HTML загружен
+
+  setTimeout(function() { // таймер-планировщик
+    document.getElementById('title').click(); // вызвать клик на кнопку
+  }, 2000); // через две секунды
+
+});
+
+
+function doFunction() { // функция привязанная к клику на кнопку
+  alert('Да, это работает!');
+}
