fc6bb2b9dbe791687c2fa0488cd6f0f696265779
nikolaytuchin <63854942+nikolaytuchin@users.noreply.github.com>
2020-06-15 16:37:20 +0500

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,14 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() { // когда весь HTML загружен
-
-  setTimeout(function() { // таймер-планировщик
-    document.getElementById('title').click(); // вызвать клик на кнопку
-  }, 2000); // через две секунды
-
-});
-
-
-function doFunction() { // функция привязанная к клику на кнопку
-  alert('Да, это работает!');
-}
-
-h1.style.cssText = 'color: pink;';
+document.h1.classList.add('article');
